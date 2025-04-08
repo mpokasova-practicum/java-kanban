c9366bc4d344200b8946ee505a96743abb5362e4
Maria Pokasova <mpokasova99@gmail.com>
2025-04-08 23:49:15 +0300

    version2 sprint5

--- src/main/manager/InMemoryHistoryManager.java
+++ src/main/manager/InMemoryHistoryManager.java
@@ -5,16 +5,22 @@ import main.model.Task;
 import java.util.ArrayList;
 
 public class InMemoryHistoryManager implements HistoryManager{
+    public static final int MAX_HISTORY_SIZE = 10;
     ArrayList<Task> history = new ArrayList<>();
 
     @Override
     public void add(Task task){
-        if (history.size() == 10) {
-            history.remove(0);
+        if (task != null) {
+            if (history.size() == MAX_HISTORY_SIZE) {
+                history.remove(0);
+            }
+            Task tempTask = new Task(task.getName(), task.getDescription(), task.getId());
+//            tempTask.setStatus(task.getStatus());
+//            А почему статус устанавливать не нужно? В Task я делала конструктор с 3 параметрами (name, description и id),
+//            а статус он сам проставляет NEW. Тогда получится, что статус у tempTask будет NEW, а у task может быть другой.
+//            Или нужно конструктор тогда переделать, добавив статус как параметр?
+            history.add(tempTask);
         }
-        Task tempTask = new Task(task.getName(), task.getDescription(), task.getId());
-        tempTask.setStatus(task.getStatus());
-        history.add(tempTask);
     }
 
     @Override
